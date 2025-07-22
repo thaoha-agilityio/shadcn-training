71d0df63676b0e1c0aa108bf5b7244f51257f36c
Thao Ha <thao.ha@asnet.com.vn>
2025-07-22 17:57:03 +0700

    feat: implement icon components

---
+++ bank-dashboard/src/components/icons/HomeIcon.tsx
@@ -0,0 +1,28 @@
+import { SVGProps } from 'react';
+
+export const HomeIcon = ({
+  width = 25,
+  height = 25,
+  color = '#B1B1B1',
+  ...props
+}: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={width}
+    height={height}
+    fill="none"
+    {...props}
+  >
+    <g>
+      <path
+        fill={color}
+        d="m24.325 10.874-.001-.002L14.126.674A2.286 2.286 0 0 0 12.498 0c-.614 0-1.192.24-1.627.674L.678 10.867l-.01.01a2.304 2.304 0 0 0 .004 3.25 2.288 2.288 0 0 0 1.598.675h.407v7.505A2.697 2.697 0 0 0 5.37 25h3.99a.733.733 0 0 0 .732-.732v-5.884a1.23 1.23 0 0 1 1.229-1.23h2.353a1.23 1.23 0 0 1 1.229 1.23v5.884c0 .404.328.732.732.732h3.99a2.697 2.697 0 0 0 2.694-2.693v-7.505h.377c.614 0 1.192-.24 1.627-.675a2.305 2.305 0 0 0 .001-3.253Z"
+      />
+    </g>
+    <defs>
+      <clipPath>
+        <path fill="#fff" d="M0 0h25v25H0z" />
+      </clipPath>
+    </defs>
+  </svg>
+);
