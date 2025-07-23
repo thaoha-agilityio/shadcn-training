0b88f672b40bd6c3d8a089cf4b0a710219b71e99
Thao Ha <thao.ha@asnet.com.vn>
2025-07-23 17:55:42 +0700

    feat: remove redundant code

--- bank-dashboard/src/app/page.tsx
+++ bank-dashboard/src/app/page.tsx
@@ -1,7 +1,14 @@
+import { Button, Input } from '../components/ui';
+import { Search } from 'lucide-react';
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      Hello
+      <Input
+        startContent={<Search width={20} height={20} />}
+        placeholder="Search"
+        variant="subtle"
+      />
+      <Input label="Label" placeholder="Search" />
     </div>
   );
 }
