40831806b8f38095c011b496d56336809d414b6a
Thao Ha <thao.ha@asnet.com.vn>
2025-07-25 11:16:59 +0700

    feat: implement DatePicker component

---
+++ bank-dashboard/src/components/DatePicker/index.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import { CalendarIcon } from 'lucide-react';
+import { format } from 'date-fns';
+import { cn } from '@/lib/utils';
+
+// Components
+import { Popover, PopoverContent, PopoverTrigger } from '../ui/Popover';
+import { Button } from '../ui/Button';
+import { Calendar } from '../ui/Calendar';
+
+interface DatePickerProps {
+  value: Date | undefined;
+  onSelect: (date: Date | undefined) => void;
+}
+
+export const DatePicker = ({ value, onSelect }: DatePickerProps) => (
+  <Popover>
+    <PopoverTrigger asChild>
+      <Button
+        variant="outline"
+        className={cn(
+          'w-full pl-3 text-left font-normal border-input-ring rounded-lg h-10 text-helper',
+        )}
+      >
+        {value ? format(value, 'PPP') : 'Date of birth'}
+        <CalendarIcon className="ml-auto h-4 w-4 opacity-50" color="#718EBF" />
+      </Button>
+    </PopoverTrigger>
+    <PopoverContent className="w-auto p-0" align="start">
+      <Calendar
+        selected={value}
+        onSelect={onSelect}
+        mode="single"
+        disabled={(date) => date > new Date() || date < new Date('1900-01-01')}
+      />
+    </PopoverContent>
+  </Popover>
+);
