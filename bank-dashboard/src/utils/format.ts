88c3dba431abbb0d012b9f594934befd213d449e
Thao Ha <thao.ha@asnet.com.vn>
2025-07-28 17:57:58 +0700

    feat: implement MyCard component

--- bank-dashboard/src/utils/format.ts
+++ bank-dashboard/src/utils/format.ts
@@ -3,3 +3,13 @@ export const formatPrice = (value: number) =>
     style: 'currency',
     currency: 'USD',
   }).format(value);
+
+export const formatCardNumber = (cardNumber: string) => {
+  const clean = cardNumber.replace(/\D/g, ''); // remove non-digits
+
+  if (clean.length !== 16) {
+    throw new Error('Card number must be 16 digits');
+  }
+
+  return [clean.slice(0, 4), '****', '****', clean.slice(12)].join(' ');
+};
