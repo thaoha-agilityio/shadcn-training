9546f51800a914bdfe339daf6aa9e47a182a3f94
Thao Ha <thao.ha@asnet.com.vn>
2025-07-30 09:54:27 +0700

    feat: add config jest

--- bank-dashboard/jest.setup.ts
+++ bank-dashboard/jest.setup.ts
@@ -1,1 +1,14 @@
 import '@testing-library/jest-dom';
+
+jest.mock('next/navigation', () => ({
+  usePathname: jest.fn().mockReturnValue('/appointments'),
+  useSearchParams: jest.fn(() => ({
+    get: jest.fn(),
+    set: jest.fn(),
+    delete: jest.fn(),
+  })),
+  useRouter: jest.fn(() => ({
+    push: jest.fn(),
+    replace: jest.fn(),
+  })),
+}));
