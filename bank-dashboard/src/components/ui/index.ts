40e9110a396323f56de599bb6aa95ac5d1b6b5a5
thao.ha <thao.ha@asnet.com.vn>
2025-08-05 00:27:17 +0700

    feat: implement transaction and edit form component

--- bank-dashboard/src/components/ui/index.ts
+++ bank-dashboard/src/components/ui/index.ts
@@ -2,3 +2,7 @@ export * from './Input';
 export * from './Switch';
 export * from './Avatar';
 export * from './Button';
+
+// Skeleton
+export * from './Skeleton/TransactionTableSkeleton';
+export * from './Skeleton/EditProfileSkeleton';
